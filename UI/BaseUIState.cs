18cc4d0940446d06b56f0ea9f3840b787a714507
xKirtle <xkirtle26@gmail.com>
2021-02-01 21:04:27 +0000

    UI Refactor

--- UI/BaseUIState.cs
+++ UI/BaseUIState.cs
@@ -5,13 +5,19 @@ using Terraria.GameContent.UI.Elements;
 using Microsoft.Xna.Framework;
 using Loadouts.Commands;
 using Loadouts.Utils;
+using Terraria.Audio;
+using Terraria.ID;
 
 namespace Loadouts.UI
 {
     class BaseUIState : UIState
     {
-        public UIImage loadoutBar;
-        public UIText loadoutText;
+        private UIImage background;
+        private UIText loadoutText;
+        private UIImage[] elements;
+        private string[] names = new string[] {"Minus", "Left", "Right", "Plus"};
+        private int[] offset = new int[] {4, 38, 96, 130};
+
         //Might want to make this a ModConfig variable?
         public static readonly int maxLoadouts = 10;
         public static int uiPosConfig;
@@ -19,31 +25,56 @@ namespace Loadouts.UI
 
         void CreateLayout()
         {
-            loadoutBar = new UIImage(ModContent.GetTexture("Loadouts/Textures/LoadoutBar"));
-            loadoutBar.Width.Set(98f, 0);
-            loadoutBar.Height.Set(31f, 0);
-            loadoutBar.Left.Set(Main.screenWidth - 165f, 0);
-            loadoutBar.Top.Set(Main.screenHeight / 2f + 350f, 0);
-            loadoutBar.OnMouseDown += (element, listener) =>
-            {
-                Vector2 barPos = new Vector2(loadoutBar.Left.Pixels, loadoutBar.Top.Pixels);
-                Vector2 clickPos = Vector2.Subtract(element.MousePosition, barPos);
+            elements = new UIImage[4];
+            const string texturePath = "Loadouts/Textures/";
 
-                if (clickPos.X > 5f && clickPos.X < 95f && clickPos.Y > 4f && clickPos.Y < 27f)
-                {
-                    if (clickPos.X <= 40f && clickPos.X <= 25f) Click(0);       //Minus
-                    else if (clickPos.X <= 40f && clickPos.X >= 25f) Click(1);  //Left
-                    else if (clickPos.X >= 60f && clickPos.X <= 75f) Click(2);  //Right
-                    else if (clickPos.X >= 60f && clickPos.X >= 75f) Click(3);  //Plus
-                }
+            background = new UIImage(ModContent.GetTexture(texturePath + "Background"))
+            {
+                Width = {Pixels = 162},
+                Height = {Pixels = 36},
+                Left = {Pixels = Main.screenWidth - 165f},
+                Top = {Pixels = Main.screenHeight / 2f + 350f}
             };
 
-            loadoutText = new UIText("");
-            loadoutText.Left.Set(45f, 0);
-            loadoutText.Top.Set(6f, 0);
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int index = i;
+                UIImage temp = new UIImage(ModContent.GetTexture(texturePath + names[index]))
+                {
+                    Width = {Pixels = 28},
+                    Height = {Pixels = 28},
+                    Left = {Pixels = offset[index]},
+                    Top = {Pixels = 4}
+                };
+                temp.OnClick += (__, _) => Click(index);
+                temp.OnMouseOver += (__, _) => Hover(index, true);
+                temp.OnMouseOut += (__, _) => Hover(index, false);
+                background.Append(temp);
 
-            loadoutBar.Append(loadoutText);
-            Append(loadoutBar);
+                elements[index] = temp;
+            }
+
+            loadoutText = new UIText("")
+            {
+                Width = {Pixels = 12},
+                Height = {Pixels = 18},
+                Left = {Pixels = 75},
+                Top = {Pixels = 9}
+            };
+            background.Append(loadoutText);
+            Append(background);
+        }
+
+        private void Hover(int index, bool mouseOver)
+        {
+            string texturePath = "Loadouts/Textures/" + names[index];
+            if (mouseOver)
+            {
+                SoundEngine.PlaySound(SoundID.MenuTick);
+                texturePath += "Hover";
+            }
+
+            elements[index].SetImage(ModContent.GetTexture(texturePath));
         }
 
         public static void Click(int index)
@@ -57,27 +88,36 @@ namespace Loadouts.UI
                 case 0: //Minus
                     if (!ConfirmDelete.deleteRequest && mp.loadouts.Count > 1)
                     {
-                        Main.NewText("Are you sure you want to remove this loadout? Every item in the loadout will be deleted. Type \"/confirm\" to remove it.", Color.Red);
+                        SoundEngine.PlaySound(SoundID.MenuTick);
+                        Main.NewText(
+                            "Are you sure you want to remove this loadout? Every item in the loadout will be deleted. Type \"/confirm\" to remove it.",
+                            Color.Red);
                         ConfirmDelete.deleteRequest = true;
                     }
+
                     break;
                 case 1: //Left
                     if (mp.loadoutIndex > 0)
                     {
+                        SoundEngine.PlaySound(SoundID.MenuTick);
                         mp.loadouts[mp.loadoutIndex].SaveLoadout();
                         mp.loadouts[--mp.loadoutIndex].LoadLoadout();
                     }
+
                     break;
                 case 2: //Right
                     if (mp.loadoutIndex < mp.loadouts.Count - 1)
                     {
+                        SoundEngine.PlaySound(SoundID.MenuTick);
                         mp.loadouts[mp.loadoutIndex].SaveLoadout();
                         mp.loadouts[++mp.loadoutIndex].LoadLoadout();
                     }
+
                     break;
                 case 3: //Plus
                     if (mp.loadouts.Count < maxLoadouts)
                     {
+                        SoundEngine.PlaySound(SoundID.MenuTick);
                         mp.loadouts[mp.loadoutIndex].SaveLoadout();
 
                         Loadout loadout = new Loadout();
@@ -86,6 +126,7 @@ namespace Loadouts.UI
 
                         mp.loadouts[mp.loadoutIndex].LoadLoadout();
                     }
+                    
                     break;
             }
         }
@@ -94,26 +135,30 @@ namespace Loadouts.UI
         {
             if (Main.playerInventory)
             {
-                if (loadoutBar == null) CreateLayout();
-                else if (loadoutBar.IsMouseHovering) Main.LocalPlayer.mouseInterface = true;
+                if (background == null) CreateLayout();
+                else if (background.IsMouseHovering) Main.LocalPlayer.mouseInterface = true;
 
                 loadoutText.SetText(Main.LocalPlayer.GetModPlayer<ELPlayer>().loadoutIndex.ToString());
 
                 if (uiPosConfig == 0)
                 {
-                    loadoutBar.Left.Set(Main.screenWidth - 165f, 0);
+                    background.Left.Set(Main.screenWidth - 165f, 0);
                     float offsetY = Main.LocalPlayer.extraAccessory && Main.expertMode ? 155f : 110f;
 
                     if (Main.mapStyle == 1) offsetY += 255f;
-                    loadoutBar.Top.Set(offsetY, 0);
+                    background.Top.Set(offsetY, 0);
                 }
                 else if (uiPosConfig == 1)
                 {
-                    loadoutBar.Left.Set(340f, 0);
-                    loadoutBar.Top.Set(265f, 0);
+                    background.Left.Set(280f, 0);
+                    background.Top.Set(265f, 0);
                 }
             }
-            else { loadoutBar?.Remove(); loadoutBar = null; }
+            else
+            {
+                background?.Remove();
+                background = null;
+            }
         }
     }
 }
